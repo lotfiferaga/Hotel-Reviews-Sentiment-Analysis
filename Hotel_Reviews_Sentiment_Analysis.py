2604a7cf0110fef0a89e5e0801768e39416537af
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2024-02-18 13:24:16 +0100

    Update Hotel_Reviews_Sentiment_Analysis.py

--- Hotel_Reviews_Sentiment_Analysis.py
+++ Hotel_Reviews_Sentiment_Analysis.py
@@ -1,1 +1,10 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+sentiments = SentimentIntensityAnalyzer()
+
+data = pd.read_csv("hotel_reviews.csv")
+print(data.head())
+
 
