f892ee71c331f282a24b8320bd872ce08e9de900
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2024-02-22 20:53:04 +0100

    Update Hotel_Reviews_Sentiment_Analysis.py

--- Hotel_Reviews_Sentiment_Analysis.py
+++ Hotel_Reviews_Sentiment_Analysis.py
@@ -9,3 +9,16 @@ print(data.head())
 
 
 
+ratings = data["Rating"].value_counts()
+numbers = ratings.index
+quantity = ratings.values
+
+custom_colors = ["skyblue", "yellowgreen", 'tomato', "blue", "red"]
+plt.figure(figsize=(5, 5))
+plt.pie(quantity, labels=numbers, colors=custom_colors)
+central_circle = plt.Circle((0, 0), 0.5, color='white')
+fig = plt.gcf()
+fig.gca().add_artist(central_circle)
+plt.rc('font', size=12)
+plt.title("Hotel Reviews Ratings", fontsize=20)
+plt.show()
