9691db397a02d5977591b6c49e579b202e8b024a
Lotfi Feraga <43786211+lotfiferaga@users.noreply.github.com>
2024-02-26 19:20:18 +0100

    Update Hotel_Reviews_Sentiment_Analysis.py

--- Hotel_Reviews_Sentiment_Analysis.py
+++ Hotel_Reviews_Sentiment_Analysis.py
@@ -23,6 +23,19 @@ plt.rc('font', size=12)
 plt.title("Hotel Reviews Ratings", fontsize=20)
 plt.show()
 
+x = sum(data["Positive"])
+y = sum(data["Negative"])
+z = sum(data["Neutral"])
+
+def sentiment_score(a, b, c):
+    if (a>b) and (a>c):
+        print("Positive 😊 ")
+    elif (b>a) and (b>c):
+        print("Negative 😠 ")
+    else:
+        print("Neutral 🙂 ")
+sentiment_score(x, y, z)
+
 sentiments = SentimentIntensityAnalyzer()
 data["Positive"] = [sentiments.polarity_scores(i)["pos"] for i in data["Review"]]
 data["Negative"] = [sentiments.polarity_scores(i)["neg"] for i in data["Review"]]
